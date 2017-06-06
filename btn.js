8e663d7ce732f9178e357250d69fe66b1dea682d
Your Name <you@example.com>
2017-06-07 00:39:04 +0200

    sss

--- btn.js
+++ btn.js
@@ -1,17 +1,3 @@
-module.exports = function(grunt) {
-
-  grunt.initConfig({
-    jshint: {
-      all: ['js/btn.js']
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  grunt.registerTask('default', ['jshint']);
-
-};
-
 var buttons = document.getElementsByClassName("btn");
 
 for(i=0;i<buttons.length;i++) {
