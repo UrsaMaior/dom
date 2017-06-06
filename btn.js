5a98b0071b3a433a9a962f564b1a94af8d9eb73a
Your Name <you@example.com>
2017-06-06 23:02:09 +0200

    kk

--- btn.js
+++ btn.js
@@ -1,3 +1,17 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: ['js/btn.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
+
 var buttons = document.getElementsByClassName("btn");
 
 for(i=0;i<buttons.length;i++) {
