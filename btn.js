b00c293c9938b5995f6704eb74b968f30b5b9547
Your Name <you@example.com>
2017-06-06 19:59:01 +0200

    bez html

--- btn.js
+++ btn.js
@@ -1,10 +1,3 @@
-<button class="btn">Hello!1</button>
-<button class="btn">Hello!2</button>
-<button class="btn">Hello!3</button>
-<button class="btn">Hello!4</button>
-<button class="btn">Hello!5</button>
-
-
 var buttons = document.getElementsByClassName("btn");
 
 for(i=0;i<buttons.length;i++) {
